51c81de4e94b195f1e1003bdbc31f3c174e6d372
DoDat_12 <89787228+DoDat-12@users.noreply.github.com>
2023-11-07 18:22:05 +0700

    another updating...

--- src/main/java/com/manager/payment_manager/App.java
+++ src/main/java/com/manager/payment_manager/App.java
@@ -1,16 +1,13 @@
 package com.manager.payment_manager;
 
-import com.manager.payment_manager.Views.ViewFactory;
+import com.manager.payment_manager.Models.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 // TODO: FILE App.java NÀY KHÔNG THÊM BẤT CỨ CÁI GÌ NỮA
 public class App extends Application {
-    public static void main(String[] args) {
-        launch();
-    }
     @Override
     public void start(Stage stage) {
-        ViewFactory.getInstance();
+        Model.getInstance().getViewFactory().showLoginWindow();
     }
 }
--- src/main/java/com/manager/payment_manager/Controllers/LoginController.java
+++ src/main/java/com/manager/payment_manager/Controllers/LoginController.java
@@ -1,4 +1,20 @@
 package com.manager.payment_manager.Controllers;
 
-public class LoginController {
+import com.manager.payment_manager.Models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    public Button login_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_btn.setOnAction(e -> {
+            Model.getInstance().getViewFactory().showClientWindow();
+        });
+    }
 }
