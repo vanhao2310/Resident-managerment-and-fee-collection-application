4b62228e631804ba6ece8126d658ae619a9ad06f
DoDat_12 <89787228+DoDat-12@users.noreply.github.com>
2023-12-19 19:43:46 +0700

    - Add ManagerStatistic and some fxml...

--- src/main/java/com/manager/payment_manager/Controllers/Manager/FeeListController.java
+++ src/main/java/com/manager/payment_manager/Controllers/Manager/FeeListController.java
@@ -1,4 +1,12 @@
 package com.manager.payment_manager.Controllers.Manager;
 
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
 public class FeeListController {
+    public Label id;
+    public Label name;
+    public Label address;
+    public Label status;
+    public Hyperlink details;
 }
