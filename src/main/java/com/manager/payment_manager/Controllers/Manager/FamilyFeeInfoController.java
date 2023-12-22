448628c88f40b935a650d310f16064cdcc95be32
howr23 <vanhao2310@gmail.com>
2023-12-22 14:38:56 +0700

    thêm tính năng nộp tiền cho khoản thu bắt buộc

--- src/main/java/com/manager/payment_manager/Controllers/Manager/FamilyFeeInfoController.java
+++ src/main/java/com/manager/payment_manager/Controllers/Manager/FamilyFeeInfoController.java
@@ -1,6 +1,9 @@
 package com.manager.payment_manager.Controllers.Manager;
 
 import com.manager.payment_manager.Models.Family;
+import com.manager.payment_manager.Models.FeeLog;
+import com.manager.payment_manager.Models.Model;
+import com.manager.payment_manager.Services.FeeLogService;
 import com.manager.payment_manager.Services.FeeService;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class FamilyFeeInfoController implements Initializable {
@@ -18,11 +22,19 @@ public class FamilyFeeInfoController implements Initializable {
     public Label money_lbl;
     public Button done_fee_btn;
     public Button cancel_btn;
+    private int feeId;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         done_fee_btn.setOnAction(actionEvent -> {
             // TODO: UPDATE LOG VÀO DATABASE
+            FeeLog feeLog = new FeeLog(Integer.parseInt(id_ho_lbl.getText()), this.feeId, Integer.parseInt(money_lbl.getText()),
+                    1, java.sql.Date.valueOf(LocalDate.now()));
+            FeeLogService.addKhoanThuHo(feeLog);
+            Model.getInstance().getViewFactory().updateManagerManaging(type_lbl.getText(), 1);
+            Stage stage = (Stage) cancel_btn.getScene().getWindow();
+            stage.close();
+
         });
 
         cancel_btn.setOnAction(actionEvent -> {
@@ -38,5 +50,6 @@ public class FamilyFeeInfoController implements Initializable {
         type_lbl.setText(feeName);
         // TODO: Load money
         money_lbl.setText(String.valueOf(FeeService.money(feeName, family.getFamilyId())));
+        this.feeId = FeeService.getIdByName(feeName);
     }
 }
